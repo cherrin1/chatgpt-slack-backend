1c0663c4d978892a6c3436c08779b3c6886f8f10
cherrin1 <cherrin@connexpay.com>
2025-07-17 08:50:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ from fastapi import FastAPI
 from slack_oauth import router as oauth_router
 from routes.send_message import router as send_message_router
 from routes.get_conversations import router as get_conversations_router
+from routes.get_messages import router as get_messages_router
 
 app = FastAPI()
 
 app.include_router(oauth_router)
 app.include_router(send_message_router)
 app.include_router(get_conversations_router)
+app.include_router(get_messages_router)
